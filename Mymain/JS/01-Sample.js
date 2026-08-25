6ff4cde4d955c0c3ec9c375c8b391562b894ac7e
Allx <156483803+AllxHen@users.noreply.github.com>
2026-08-25 17:12:29 -0300

    Add files

---
+++ Mymain/JS/01-Sample.js
@@ -0,0 +1,196 @@
+-- Cada Numero é um codigo novo, e todos os dias estarei atualizando conforme meu aprendizado!
+
+
+--1
+console.log("Ola mundo!");
+
+--2
+console.log("Meu nome é allex");
+console.log("Tenho 18 anos")
+console.log("Estou aprendendo JavaScript!");
+console.log(30);
+console.log(5+8);
+
+--3
+let nome = ("Ana");
+
+--4
+const nome = ("Bob");
+let pontos = (20);
+
+console.log(nome);
+console.log(pontos);
+
+pontos = 10;
+console.log(pontos);
+
+
+------------------------------------------------------------
+
+
+
+--EX1 (p1,p2,p3,p4)
+
+--p1 
+console.log("allex");
+console.log("18");
+
+--p2
+const cidade = ("Um lugar por ai");
+
+--p3
+let pontos = (50);
+
+console.log(pontos);
+
+pontos = 150;
+
+console.log(pontos);
+
+--p4
+const nome = ("Gohan");
+let idade = (29);
+const curso = ("beleza")
+let cidade = ("Toki Toki");
+
+console.log(nome);
+console.log(idade);
+console.log(curso);
+console.log(cidade);
+
+
+---------------------------------------------------------
+
+
+
+--5
+let n1 = (10);
+let n2 = (20);
+let total = n1+n2;
+
+console.log(total);
+
+--6
+let n1 = (50);
+let n2 = (30);
+let total = (n1-n2);
+
+console.log(total);
+
+--7
+let nota1 = (9);
+let nota2 = (6);
+let media = (nota1+nota2)/2;
+
+console.log(media);
+
+--8
+let idadeAtual = (18);
+let anosPassados = (7);
+let idadeFutura = idadeAtual + anosPassados;
+
+console.log(idadeFutura);
+
+-------------------------------------------------------------
+
+
+
+
+--EX2 (p1,...,p12)
+
+--p1 
+const nome = ("batata");
+let idade = (3);
+
+console.log(idade);
+console.log(nome);
+
+--p2
+let n1 = (2424);
+let n2 = (2013);
+let nt = n1 + n2;
+
+console.log(nt);
+
+--p3
+let n1 = (40);
+let n2 = (50);
+let soma = n1 + n2;
+let sub = n1 - n2;
+let mult = n1*n2;
+let div = n1/n2;
+
+console.log(soma);
+console.log(sub);
+console.log(mult);
+console.log(div);
+
+--p4
+let n1 = (40);
+
+console.log(n1*2);
+
+--p5
+let n1 = (5);
+
+console.log(n1*3);
+
+--p6
+let n1 = (6);
+let n2 = (3);
+let media = (n1+n2)/2;
+
+console.log(media);
+
+--p7
+let idadeAtual = (59);
+let idadeFutura = 59 + 10;
+
+console.log(idadeFutura);
+
+--p8
+let preco = (90)
+let quantidade = (4)
+let ValorTotal = preco*quantidade;
+
+console.log(ValorTotal);
+
+--p9
+let camisa = (50)
+let quantidade = (6)
+let total = camisa*quantidade;
+
+console.log(total);
+
+--p10
+let conta = (240);
+let pessoas = (4);
+let valorPago = conta/pessoas;
+
+console.log(valorPago);
+
+--p11
+let produto = (200);
+let desconto = (30);
+let realDesconto = produto - desconto;
+
+console.log(realDesconto);
+
+--p12
+let salario = (2500);
+let internet = (100);
+let alimentacao = (600);
+let transporte = (300);
+let totalGasto = internet + alimentacao + transporte;
+let finalMes = salario - totalGasto;
+
+console.log("total gasto:" +totalGasto+ ", sobrou:" +finalMes);
+
+
+
+
+
+
+
+
+
