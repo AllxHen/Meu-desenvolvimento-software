cf794b5a2f78f4b208a0b1ed24e00c848ecc3990
Allx <156483803+AllxHen@users.noreply.github.com>
2026-09-11 23:44:29 -0300

    Update 01-IfElse.js

--- Mymain/JS/01-IfElse.js
+++ Mymain/JS/01-IfElse.js
@@ -347,10 +347,10 @@ const prompt = require('prompt-sync')()
 console.log ("Festa")
 
 let idade = Number(prompt("Qual sua idade?:"))
-let convite = prompt("Voce tem convite?:").trim()
-if (idade >= 18 && convite=== "sim") {
-console.log ("Bem vindo a festa")
-} 
+let convite = prompt("Voce tem convite?:")
+
+if (idade >= 18 && convite === "sim") {
+console.log ("Bem vindo a festa")} 
 else{console.log ("entrada nao autorizada")}
 
 --22
