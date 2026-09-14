397f60f7d9a08c6949f6dbce77e41aa7c1039e1a
Allx <156483803+AllxHen@users.noreply.github.com>
2026-09-14 18:00:32 -0300

    Add login validation script

---
+++ js/elif/008.js
@@ -0,0 +1,7 @@
+let usuario = "admin"
+let senha = "1234"
+
+if (usuario === "admin" && senha === "1234"){
+    console.log("login realizado com sucesso")
+}
+else {console.log("usuario ou senha incorretos!")}
