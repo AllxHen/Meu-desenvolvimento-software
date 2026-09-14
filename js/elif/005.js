fdf108e33115842cde40b1270cd81f18819af9f4
Allx <156483803+AllxHen@users.noreply.github.com>
2026-09-14 17:32:02 -0300

    Add discount calculation based on purchase amount

---
+++ js/elif/005.js
@@ -0,0 +1,14 @@
+let compra = 150
+let total = 0
+
+if (compra >= 200){
+    total = compra*0.80
+    console.log(`20% de desconto! valor total: ${total}`)
+    
+}
+else if (compra >= 100){
+    total = compra*0.90
+    console.log(`10% de desconto! valor total: ${total}`)
+    
+}
+else {console.log("sem desconto")}
